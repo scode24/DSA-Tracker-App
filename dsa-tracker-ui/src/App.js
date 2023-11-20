2bf8ddca68b7e23c270a0bfd3b6f21b44c032fc8
scode24 <sarkar.soumyabrata2@gmail.com>
2023-11-20 22:07:19 +0530

    Handled * route

--- dsa-tracker-ui/src/App.js
+++ dsa-tracker-ui/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header';
-import Main from './components/Main';
-import Login from './components/Login';
-import Register from './components/Register';
-import ForgotPassword from './components/ForgotPassword';
-import RouteGuard from './shared/RouteGuard';
-import Playground from './components/Playground';
-import About from './components/About';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ForgotPassword from "./components/ForgotPassword";
+import RouteGuard from "./shared/RouteGuard";
+import Playground from "./components/Playground";
+import About from "./components/About";
 
 function App() {
   return (
@@ -15,12 +15,13 @@ function App() {
       <Header></Header>
       <Routes>
         {/* <Route path='/' element={<RouteGuard />}> */}
-        <Route path='/' element={<Main />} />
-        <Route path='login' element={<Login />} />
-        <Route path='register' element={<Register />} />
-        <Route path='forgotPassword' element={<ForgotPassword />} />
-        <Route path='playground' element={<Playground />} />
-        <Route path='about' element={<About />} />
+        <Route path="/" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="forgotPassword" element={<ForgotPassword />} />
+        <Route path="playground" element={<Playground />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
         {/* </Route> */}
       </Routes>
     </BrowserRouter>
